cbec1a7b05bb7d6f5c7b11ca365af6d53239e564
Sean Gillespie <info@spg.business>
2021-11-30 02:07:40 -0500

    Revert to unlinted import order in model.py

--- model.py
+++ model.py
@@ -1,30 +1,36 @@
-import torch
-import torch.utils.data
-from torchvision.transforms import functional as TF
-from torchvision import datasets, transforms, utils
-from IPython import display
-from lib.util import pil_from_tensor, pil_to_tensor
-from lib import util
-from lib.script_util import create_model_and_diffusion, model_and_diffusion_defaults
-import clip_jax
-from tqdm.notebook import tqdm
-from jaxtorch import PRNG, Context, Module, nn, init
-import jaxtorch
-import jax.numpy as jnp
-import jax
-import numpy as np
-import requests
-from PIL import Image
-from dataclasses import dataclass
-from functools import partial
-import functools
-import os
-import time
-import io
-import math
 import sys
 sys.path.append('./CLIP_JAX')
 sys.path.append('./jax-guided-diffusion')
+
+import math
+import io
+import time
+import os
+import functools
+from functools import partial
+from dataclasses import dataclass
+
+from PIL import Image
+import requests
+
+import numpy as np
+import jax
+import jax.numpy as jnp
+import jaxtorch
+from jaxtorch import PRNG, Context, Module, nn, init
+from tqdm.notebook import tqdm
+
+import clip_jax
+
+from lib.script_util import create_model_and_diffusion, model_and_diffusion_defaults
+from lib import util
+from lib.util import pil_from_tensor, pil_to_tensor
+
+from IPython import display
+from torchvision import datasets, transforms, utils
+from torchvision.transforms import functional as TF
+import torch.utils.data
+import torch
 
 devices = jax.devices()
 n_devices = len(devices)
