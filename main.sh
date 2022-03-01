eeaa3a0ce82f20995b75c0b5c5be61bfb50f5c4d
Sean Gillespie <info@spg.business>
2022-03-01 12:21:02 -0500

    Add main.sh piping incoming single json param to model.py args

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This is a dirty hack to let us pass arbitrary arguments to clipit.py as a single string.
+
+# The user of this file will call it like this
+# CMD ["main.sh", "{ \"prompt\": \"Big Al's best friend\", \"iterations\": 300}"]
+# Where the only argument to this script ($1) is a json string with key-value argument value pairs.
+
+# The first python script will turn this into
+# --prompt
+# Big Al's best friend
+# --iterations
+# 300
+
+# And xarg will glue those together into a single command
+printf "Received input:\n"
+printf "%s\n" "$1"
+python json_to_bash_args.py "$1" | xargs -0 -d "\n" python model.py
